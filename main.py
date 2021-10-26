09d380a2bfbc29b09750b46cd7aec9200bcdc538
Leo <leo@battin.dev>
2021-10-26 10:54:41 +0200

    Perspective transformation

--- main.py
+++ main.py
@@ -49,8 +49,33 @@ class MainWidget(Widget):
         spacing = self.V_LINES_SPACING * self.width
         for i in range(0, self.V_LINES_NB):
             line_x = int(central_line_x + offset*spacing)
-            self.vertical_lines[i].points = [line_x, 0, line_x, self.height]
+
+            x1, y1 = self.transform(line_x, 0)
+            x2, y2 = self.transform(line_x, self.height)
+            self.vertical_lines[i].points = [x1, y1, x2, y2]
+
             offset += 1
+
+    def transform(self, x, y):
+        # return self.transform_2D(x, y)
+        return self.transform_perspective(x, y)
+
+    # noinspection PyPep8Naming,PyMethodMayBeStatic
+    def transform_2D(self, x, y):
+        return int(x), int(y)
+
+    def transform_perspective(self, x, y):
+        tr_y = y / self.height * self.perspective_point_y
+        if tr_y > self.perspective_point_y:
+            tr_y = self.perspective_point_y
+
+        diff_x = x - self.perspective_point_x
+        diff_y = self.perspective_point_y - tr_y
+        proportion_y = diff_y / self.perspective_point_y
+
+        tr_x = self.perspective_point_x + diff_x * proportion_y
+
+        return int(tr_x), int(tr_y)
 
 
 class GalaxyApp(App):
